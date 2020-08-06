5d4f3c1a16a2c4f47d8ea78f6ad8e0f595540a93
Aman Singh Aswal <amanpotato11@gmail.com>
2020-08-06 16:51:20 +0530

    started with the hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from "react";
-import {render} from "react-dom";
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,10 @@
 import React from "react";
 export default function Pet({ name, animal, breed }) {
-    return (
-        <div>
-            <h1>{name}</h1>
-            <h2>{animal}</h2>
-            <h2>{breed}</h2>    
-        </div>
-    );
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
